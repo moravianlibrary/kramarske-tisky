42549d808c1acda567165e7c7701c953bddda0b0
rzeh4n <martin.rehanek@gmail.com>
2024-11-19 16:39:15 +0100

    fix listing files and sorting

--- src/main/java/cz/trinera/dkt/DigitizationWorkflow.java
+++ src/main/java/cz/trinera/dkt/DigitizationWorkflow.java
@@ -3,6 +3,9 @@ package cz.trinera.dkt;
 import cz.trinera.dkt.barcode.BarcodeDetector;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Objects;
 
 public class DigitizationWorkflow {
 
@@ -12,12 +15,24 @@ public class DigitizationWorkflow {
         this.barcodeDetector = barcodeDetector;
     }
 
+    /**
+     * Reads file names from inputDir, sorts them by number and processes them
+     * File must be named as a number with a .png extension
+     *
+     * @param inputDir directory with png files
+     */
     public void run(File inputDir) {
         System.out.println("Running digitization workflow on " + inputDir);
         //process all png files in the directory inputDir
-        for (File file : inputDir.listFiles((dir, name) -> name.endsWith(".png"))) {
+        for (File file : Arrays.stream(Objects.requireNonNull(
+                inputDir.listFiles((dir, name) -> name.matches("\\d+\\.png")))
+        ).sorted((first, second) -> {
+            int nFirst = Integer.parseInt(first.getName().split("\\.")[0]);
+            int nSecond = Integer.parseInt(second.getName().split("\\.")[0]);
+            return Integer.compare(nFirst, nSecond);
+        }).toArray(File[]::new)) {
             if (file.getName().endsWith(".png")) {
-                System.out.println("Processing " + file);
+                System.out.println("Processing " + file.getName());
                 BarcodeDetector.Barcode barcode = barcodeDetector.detect(file);
                 if (barcode != null) {
                     System.out.println("Found barcode: " + barcode.getFormat() + " " + barcode.getValue());
