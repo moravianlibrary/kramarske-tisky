f6828eafc714f6b6a76e84c54099d1d8605e536d
rzeh4n <martin.rehanek@gmail.com>
2024-12-20 16:51:10 +0100

    extend Pero connector to upload images

--- src/main/java/cz/trinera/dkt/ocr/PeroHelper.java
+++ src/main/java/cz/trinera/dkt/ocr/PeroHelper.java
@@ -2,6 +2,7 @@ package cz.trinera.dkt.ocr;
 
 import cz.trinera.dkt.utils.ApiResponse;
 import cz.trinera.dkt.utils.MyApacheHttpClientProvider;
+import org.apache.hc.client5.http.entity.mime.MultipartEntityBuilder;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
 import org.apache.hc.core5.http.ClassicHttpRequest;
@@ -14,6 +15,7 @@ import org.apache.hc.core5.http.io.support.ClassicRequestBuilder;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Base64;
 
@@ -121,14 +123,56 @@ public class PeroHelper {
         }
     }
 
+    public ApiResponse queryPostMultipart(String urlPath, File inFile) throws IOException {
+        try (CloseableHttpClient httpClient = MyApacheHttpClientProvider.getClient()) {
+            String url = buildUrl(urlPath);
+            System.out.println("url: " + url);
+
+            // Create the multipart entity with the file
+            HttpEntity multipartEntity = MultipartEntityBuilder.create()
+                    .addBinaryBody("file", inFile, ContentType.DEFAULT_BINARY, inFile.getName())
+                    .build();
+            ClassicHttpRequest httpPost = ClassicRequestBuilder.post(url)
+                    //.setEntity(new StringEntity(body.toString(), ContentType.APPLICATION_JSON))
+                    .setEntity(multipartEntity)
+                    .setHeader("api-key", this.apiKey)
+                    .build();
+            long start = System.currentTimeMillis();
+            try (CloseableHttpResponse response = httpClient.execute(httpPost)) {
+                float durationS = (System.currentTimeMillis() - start) / 1000f;
+                //System.out.println(response.getCode() + " " + response.getReasonPhrase());
+                HttpEntity entity = response.getEntity();
+                if (response.getCode() < 300) { //ok
+                    String responseBody = EntityUtils.toString(entity);
+                    //System.out.println("Response: " + responseBody);
+                    EntityUtils.consume(entity);//to make sure the connection is released
+                    return new ApiResponse(url, "POST", null, response.getCode(), new JSONObject(responseBody), durationS);
+                } else {
+                    JSONObject errorJson = null;
+                    String responseBodyStr = EntityUtils.toString(entity);
+                    try {
+                        errorJson = new JSONObject(responseBodyStr);
+                    } catch (JSONException e) {
+                        String responseMessage = response.getReasonPhrase();
+                        if (responseMessage != null && !responseMessage.trim().isEmpty()) {
+                            errorJson = new JSONObject();
+                            errorJson.put("error", responseMessage);
+                        }
+                    }
+                    System.err.println("Error: " + response.getCode() + " - " + errorJson);
+                    EntityUtils.consume(entity);//to make sure the connection is released
+                    return new ApiResponse(url, "POST", null, response.getCode(), null, errorJson, durationS);
+                }
+            } catch (ParseException e) {
+                throw new IOException(e);
+            }
+        } finally {
+            //MyApacheHttpClientProvider.cleanup();
+        }
+    }
+
     private String buildUrl(String urlPath) {
         return this.baseUrl + "/" + urlPath;
     }
-
-    /*private String buildAuthorizationString() {
-        String auth = Config.instanceOf().getEsLogin() + ":" + Config.instanceOf().getEsPassword();
-        byte[] encodedAuth = Base64.getEncoder().encode(auth.getBytes());
-        return "Basic " + new String(encodedAuth);
-    }*/
 
 }
--- src/test/java/cz/trinera/dkt/PeroHelperTest.java
+++ src/test/java/cz/trinera/dkt/PeroHelperTest.java
@@ -5,7 +5,9 @@ import cz.trinera.dkt.utils.ApiResponse;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,6 +16,7 @@ public class PeroHelperTest {
     private final boolean testsDisabled = true;
     private final String baseUrl = "https://pero-ocr.fit.vutbr.cz/api";
     private final String apiKey = "API_KEY";
+    private final String sampleDir = new File(System.getProperty("user.home")).getAbsolutePath() + "/TrineraProjects/KramarskeTisky/data/input/ocr-test";
 
     private final PeroHelper peroHelper = new PeroHelper(baseUrl, apiKey);
 
@@ -45,7 +48,9 @@ public class PeroHelperTest {
         try {
             JSONObject data = new JSONObject();
             data.put("engine", 1);
-            data.put("images", new JSONObject());
+            JSONObject images = new JSONObject();
+            images.put("page_1", JSONObject.NULL);
+            data.put("images", images);
 
             System.out.println("request data:");
             System.out.println(data.toString(2));
@@ -59,10 +64,27 @@ public class PeroHelperTest {
             assertTrue(responseDataJson.has("request_id"));
             assertTrue(responseDataJson.has("status"));
             assertEquals("success", responseDataJson.getString("status"));
-            //b20eef74-5015-4154-9346-2a1fea1aa0f7
         } catch (IOException e) {
             fail(e);
         }
+    }
+
+    private String createProcessingRequest(int engine, List<String> pageIds) throws IOException {
+        JSONObject data = new JSONObject();
+        data.put("engine", engine);
+        JSONObject images = new JSONObject();
+        for (String pageId : pageIds) {
+            images.put(pageId, JSONObject.NULL);
+        }
+        data.put("images", images);
+        //System.out.println("request data:");
+        //System.out.println(data.toString(2));
+        ApiResponse response = peroHelper.queryPost("post_processing_request", data);
+        String responseData = response.result.toString();
+        JSONObject responseDataJson = new JSONObject(responseData);
+        //System.out.println("response data:");
+        //System.out.println(responseDataJson.toString(2));
+        return responseDataJson.getString("request_id");
     }
 
     @Test
@@ -71,7 +93,12 @@ public class PeroHelperTest {
             return;
         }
         try {
-            String requestId = "b20eef74-5015-4154-9346-2a1fea1aa0f7";
+            //create request with one page
+            String singlePageId = "page_1";
+            String requestId = createProcessingRequest(1, List.of(singlePageId));
+            System.out.println("requestId: " + requestId);
+
+            //check status
             ApiResponse response = peroHelper.queryGet("request_status/" + requestId);
             assertTrue(response.isOk());
             String responseData = response.result.toString();
@@ -82,9 +109,45 @@ public class PeroHelperTest {
             assertTrue(responseDataJson.has("status"));
             assertEquals("success", responseDataJson.getString("status"));
             assertTrue(responseDataJson.has("request_status"));
-            assertEquals("{}", responseDataJson.getJSONObject("request_status").toString());
+            JSONObject requestStatus = responseDataJson.getJSONObject("request_status");
+            assertTrue(requestStatus.has(singlePageId));
+            JSONObject page1 = requestStatus.getJSONObject(singlePageId);
+            assertTrue(page1.has("state"));
+            assertEquals("CREATED", page1.getString("state"));
+            assertTrue(page1.has("quality"));
+            assertEquals(JSONObject.NULL, page1.get("quality"));
+
         } catch (IOException e) {
             fail(e);
         }
     }
+
+    @Test
+    public void uploadImage() {
+        if (testsDisabled) {
+            return;
+        }
+        try {
+            //create request with one page
+            String pageId = "page_1";
+            String requestId = createProcessingRequest(1, List.of(pageId));
+            System.out.println("requestId: " + requestId);
+
+            //upload image
+            File inFile = new File(sampleDir + "/0005.png");
+            ApiResponse response = peroHelper.queryPostMultipart("upload_image/" + requestId + "/" + pageId, inFile);
+            System.out.println(response.toString());
+            String responseData = response.result.toString();
+            JSONObject responseDataJson = new JSONObject(responseData);
+            assertTrue(response.isOk());
+
+            System.out.println("response data:");
+            System.out.println(responseDataJson.toString(2));
+            assertTrue(responseDataJson.has("status"));
+            assertEquals("success", responseDataJson.getString("status"));
+        } catch (IOException e) {
+            fail(e);
+        }
+    }
+
 }
