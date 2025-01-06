6f863ae0c7375746f574021751e0a6980eecab63
rzeh4n <martin.rehanek@gmail.com>
2025-01-06 10:01:16 +0100

    extend ocr provider tests

--- src/main/java/cz/trinera/dkt/ocr/OcrProviderImpl.java
+++ src/main/java/cz/trinera/dkt/ocr/OcrProviderImpl.java
@@ -9,6 +9,10 @@ public class OcrProviderImpl implements OcrProvider {
 
     private final Logger logger = Logger.getLogger(OcrProviderImpl.class.getName());
 
+    //TODO: use url and api key from configuration
+    private String baseUrl = "https://pero-ocr.fit.vutbr.cz/api";
+    private String apiKey = "API_KEY";
+
     private final int engineId;
 
     public OcrProviderImpl(int engineId) {
@@ -19,6 +23,8 @@ public class OcrProviderImpl implements OcrProvider {
     @Override
     public void checkAvailable() throws ToolAvailabilityError {
         //TODO: implement
+        throw new ToolAvailabilityError("OCR provider is not available");
+        //TODO: check engines and that our engine is available
     }
 
     @Override
--- src/test/java/cz/trinera/dkt/OcrProviderTest.java
+++ src/test/java/cz/trinera/dkt/OcrProviderTest.java
@@ -8,16 +8,17 @@ import org.junit.jupiter.api.Test;
 import java.io.File;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class OcrProviderTest {
 
     private final String sampleDir = new File(System.getProperty("user.home")).getAbsolutePath() + "/TrineraProjects/KramarskeTisky/data/input/ocr-test";
 
-    OcrProvider ocrProvider = new OcrProviderMock();
-    //OcrProvider ocrProvider = new OcrProviderImpl(1);
+    //OcrProvider ocrProvider = new OcrProviderMock();
+    OcrProvider ocrProvider = new OcrProviderImpl(1);
 
     @Test
-    public void getOcr1() {
+    public void getOcr() {
         //prepare
         System.out.println(sampleDir);
         File inImageFile = new File(sampleDir + "/0001.png");
@@ -28,10 +29,23 @@ public class OcrProviderTest {
         outAltoFile.delete();
         //convert
         ocrProvider.fetchOcr(inImageFile, outTextFile, outAltoFile);
-        //check results
+        //check results - text
         assertTrue(outTextFile.exists());
-        //TODO: check size, content of outTextFile
+        long textFileBytes = outTextFile.length();
+        assertTrue(textFileBytes > 0);
+        //check restuls - alto
         assertTrue(outAltoFile.exists());
-        //TODO: check size, content of outAltoFile
+        long altoFileBytes = outAltoFile.length();
+        assertTrue(altoFileBytes > 0);
+        //TODO: parse content of alto file as xml
+    }
+
+    @Test
+    public void checkAvailable() {
+        try {
+            ocrProvider.checkAvailable();
+        } catch (ToolAvailabilityError e) {
+            fail(e);
+        }
     }
 }
